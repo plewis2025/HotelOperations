89529c695b45d7a39f8e5b462eecc8c1536e2d8a
Prosperity <lprosperity13@gmail.com>
2025-10-21 15:04:55 -0700

    updated all files

---
+++ src/Reservation.java
@@ -0,0 +1,54 @@
+public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+    private double pricePerNight;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+
+        if (roomType.equalsIgnoreCase("king")) {
+            pricePerNight = 139.00;
+        } else if (roomType.equalsIgnoreCase("double")) {
+            pricePerNight = 124.00;
+        }
+
+        if (isWeekend) {
+            pricePerNight *= 1.10; // +10% increase
+        }
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPrice() {
+        return pricePerNight;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setIsWeekend(boolean isWeekend) {
+        this.isWeekend = isWeekend;
+    }
+
+    public double getReservationTotal() {
+        return pricePerNight * numberOfNights;
+    }
+}
